327e3a3999b588fd1f6727652c5e692af66171e6
J. Nma <wooldoughnutspi@outlook.com>
2023-01-24 14:11:39 +0000

    add model interfaces

--- src/lib/models/DirectoryObject.ts
+++ src/lib/models/DirectoryObject.ts
@@ -1,31 +1,51 @@
 import { ObjectId } from 'mongodb';
-import mongoose, { type InferSchemaType } from 'mongoose';
+import mongoose, { Model, type HydratedDocument } from 'mongoose';
 import Delta, { Op } from 'quill-delta';
 const { Schema } = mongoose;
 
-const directoryObjectSchema = new Schema(
-	{
-		type: { type: String, enum: ['file', 'folder'] },
-		parent: {
-			type: ObjectId,
-			ref: 'DirectoryObject'
-		},
-		name: { type: String, required: true, trim: true },
-		content: {
-			type: Object,
-			get: (data?: Op[] | { ops: Op[] }) => new Delta(data)
-		}
+export interface IDirectoryObject {
+	type: 'file' | 'folder';
+	parent: ObjectId;
+	name: string;
+	content?: Op[] | { ops: Op[] };
+	fileId?: string;
+}
+
+export type DirectoryObjectDocument = HydratedDocument<IDirectoryObject, IDirectoryObjectMethods>;
+
+export interface IDirectoryObjectMethods {
+	findChildren(): Promise<DirectoryObjectDocument[]>;
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type DirectoryObjectModel = Model<IDirectoryObject, {}, IDirectoryObjectMethods>;
+
+const directoryObjectSchema = new Schema<
+	IDirectoryObject,
+	DirectoryObjectModel,
+	IDirectoryObjectMethods
+>({
+	type: { type: String, enum: ['file', 'folder'] },
+	parent: {
+		type: ObjectId,
+		ref: 'DirectoryObject'
 	},
-	{
-		methods: {
-			findChildren(cb) {
-				return mongoose.model('DirectoryObject').find({ parent: this._id }, cb);
-			}
-		}
+	name: { type: String, required: true, trim: true },
+	content: {
+		type: Object,
+		get: (data?: Op[] | { ops: Op[] }) => new Delta(data)
+	},
+	fileId: {
+		type: ObjectId,
+		ref: 'files'
 	}
-);
+});
 
-export type DirectoryObject = InferSchemaType<typeof directoryObjectSchema>;
+directoryObjectSchema.method('findChildren', function findChildren() {
+	return mongoose.model<IDirectoryObject>('DirectoryObject').find({ parent: this._id }).exec();
+});
 
-export default mongoose.models.DirectoryObject ||
-	mongoose.model('DirectoryObject', directoryObjectSchema);
+const model: DirectoryObjectModel =
+	mongoose.models.DirectoryObject || mongoose.model('DirectoryObject', directoryObjectSchema);
+
+export default model;
